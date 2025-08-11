13a4896bdbd0de97589e60d1bf1fc03879133a7b
SelvaGsk <SelvakumarGSK24@gmail.com>
2025-08-11 19:35:56 +0530

    SiteMap Changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import ViteSitemap from 'vite-plugin-sitemap';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), // React plugin
+    tailwindcss(), // TailwindCSS plugin for utility-first styling
+    ViteSitemap({
+      hostname: 'https://chennaisparklecrackers.com/', // Replace with your actual base URL
+      outDir: 'dist', // Output directory for sitemap
+    }),
+  ],
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
